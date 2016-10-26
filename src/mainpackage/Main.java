f5ee3deca7f23d2cf1335b0689413cbb629b2a1f
DESKTOP-AUS889I\kacper1030 <kacper.jakobczak15@onet.pl>
2016-10-26 19:21:36 +0200

    Usunięto nie potrzebną klase, dodano input

--- src/mainpackage/Main.java
+++ src/mainpackage/Main.java
@@ -4,13 +4,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Wzory wzory = new Wzory();
 		Napisy napisy = new Napisy();
 		
 		napisy.ekranPowitalny();
-		
-		wzory.wzorP();
-		
+		napisy.autor();
+		napisy.wprowadzDane();
 		
 	}
 
--- src/mainpackage/Napisy.java
+++ src/mainpackage/Napisy.java
@@ -1,7 +1,9 @@
 package mainpackage;
 
-public class Napisy {
+import java.util.Scanner;
 
+public class Napisy{
+	
 	public void ekranPowitalny(){
 		System.out.println("");
 		System.out.println("Witaj w programie!");
@@ -19,10 +21,37 @@ public class Napisy {
 	}
 	public void wprowadzDane(){
 		System.out.println("");
+		System.out.println("Spersonalizuj swoje równanie");
 		System.out.println("");
+		System.out.println("Wprowadz a");
+		
+		wpisDoZmiennejA(0);		// nastepuje wpis do zmiennej a
+		
+		System.out.println("Wprowadz b");
+		
+		wpisDoZmiennejB(0);		// nastepuje wpis do zmiennej b
+		
+		System.out.println("Wprowadz c");
+		
+		wpisDoZmiennejC(0);		// nastepuje wpis do zmiennej c
+	}
+	public void wpisDoZmiennejA(int a){ 	// wczytuje dane do zmiennej a
+		
+		Scanner odczyt = new Scanner(System.in);
+		a = odczyt.nextInt();
+		System.out.println("Wpisano pomyślnie");
 		System.out.println("");
+	}
+	public void wpisDoZmiennejB(int b){		// wczytuje dane do zmiennej b
+		Scanner odczyt = new Scanner(System.in);
+		b = odczyt.nextInt();
+		System.out.println("Wpisano pomyślnie");
 		System.out.println("");
-		System.out.println("");
+	}
+	public void wpisDoZmiennejC(int c){		// wczytuje dane do zmiennej c
+		Scanner odczyt = new Scanner(System.in);
+		c = odczyt.nextInt();
+		System.out.println("Wpisano pomyślnie");
 		System.out.println("");
 	}
 	
